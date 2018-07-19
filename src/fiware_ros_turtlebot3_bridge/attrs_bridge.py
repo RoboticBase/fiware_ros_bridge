0d0d8ab35333c155756fe750db82370e24f28523
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-07-19 12:20:33 +0900

    add 'turtlebot3_attrs' node

---
+++ src/fiware_ros_turtlebot3_bridge/attrs_bridge.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+import pytz
+
+import rospy
+
+from fiware_ros_turtlebot3_bridge.msg import r_pos
+from fiware_ros_turtlebot3_bridge.base import MQTTBase
+
+from fiware_ros_turtlebot3_bridge.logging import getLogger
+logger = getLogger(__name__)
+
+PAYLOAD_FMT = '{timestamp}|x|{x}|y|{y}|z|{z}|theta|{theta}'
+
+
+class AttrsBridge(MQTTBase):
+    def __init__(self):
+        self.__params = rospy.get_param('~')
+        super(AttrsBridge, self).__init__(self.__params)
+        rospy.Subscriber(self.__params['topics']['ros'], r_pos, self._on_receive, queue_size=10)
+        self.__attrs_topic = self.__params['topics']['mqtt']
+
+    def start(self):
+        logger.infof('AttrsBridge start')
+        rospy.spin()
+        logger.infof('AttrsBridge finish')
+
+    def _on_receive(self, msg):
+        logger.infof('received message from ros : {}', str(msg).replace('\n', ' '))
+
+        timestamp = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        msg = PAYLOAD_FMT.format(timestamp=timestamp, x=msg.x, y=msg.y, z=msg.z, theta=msg.theta)
+        self.client.publish(self.__attrs_topic, msg)
